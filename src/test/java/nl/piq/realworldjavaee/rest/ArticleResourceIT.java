3f507b4d13e56328ef3273b5d7cb6b0947a8908f
Pieter Kuijpers <pieter@pi-q.nl>
2018-11-07 20:09:50 +0100

    OpenLiberty instead of Wildfly for Arquillian test

--- src/test/java/nl/piq/realworldjavaee/rest/ArticleResourceIT.java
+++ src/test/java/nl/piq/realworldjavaee/rest/ArticleResourceIT.java
@@ -1,17 +1,20 @@
 package nl.piq.realworldjavaee.rest;
 
+import io.restassured.RestAssured;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.net.URL;
 
 import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.empty;
 
 @RunWith(Arquillian.class)
@@ -28,10 +31,21 @@ public class ArticleResourceIT {
         return archive;
     }
 
+    @Before
+    public void setUp() throws Exception {
+        RestAssured.baseURI = base.getProtocol() + "://" + base.getAuthority();
+        RestAssured.port = base.getPort();
+        RestAssured.basePath = base.getPath();
+    }
+
     @Test
     public void getArticles_withEmptyDatabase_returnsZeroArticles() {
-        get("/api/articles").then()
+        given()
+                .log().all()
+        .when()
+                .get("/api/articles")
+        .then()
                 .statusCode(200)
-                .body("articles", empty());
+                .body("articles", empty()).log();
     }
 }
